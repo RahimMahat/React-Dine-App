21a8db1c10934dbae092a7c4b78fa622468660f0
RahimMahat <rahimmahat99@gmail.com>
2021-09-26 13:00:36 +0530

    adding modal to the shopping cart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Fragment } from "react";
 // Importing header
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+// Importing cart
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./Cart.module.css";
+// importing modal
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
@@ -11,7 +13,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -21,7 +23,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
