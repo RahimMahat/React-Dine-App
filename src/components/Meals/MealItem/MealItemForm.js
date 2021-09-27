d2dd146a40dd2f6a149886254fbda1360ddfb2be
RahimMahat <rahimmahat99@gmail.com>
2021-09-27 18:02:46 +0530

    working with ref and forward ref to get the element in the cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 // importing input element
 import Input from "../../UI/Input";
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredNumAmount = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredNumAmount < 1 ||
+      enteredNumAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredNumAmount);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        lable="Amount"
+        ref={amountInputRef}
+        label="Amount"
         input={{
           id: "amount",
           type: "number",
@@ -17,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount [1-5]</p>}
     </form>
   );
 };
